cea7dae651d07901ea3bcddc6544d66a14a1cf29
valenthyne-ut <valenthyne.ut@gmail.com>
2024-05-02 09:34:13 +0100

    Fix typo

--- src/userinterface/Session/Embeds/ListNoneFound.ts
+++ src/userinterface/Session/Embeds/ListNoneFound.ts
@@ -3,6 +3,6 @@ import { Colors, EmbedBuilder } from "discord.js";
 export function ListNoneFound() {
 	return new EmbedBuilder()
 		.setColor(Colors.Purple)
-		.setThumbnail("No sessions scheduled.")
+		.setTitle("No sessions scheduled.")
 		.setDescription("This server has no sessions scheduled.");
 }
