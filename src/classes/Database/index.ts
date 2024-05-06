275a04a6adff537a63ca880b7a5c6ac941e8ee6c
valenthyne-ut <valenthyne.ut@gmail.com>
2024-05-06 15:30:46 +0100

    Bug-fixing
    Fixed an issue where the bot would only send reminders to the first
    server it's in. (caused by a typo, where an assigment was in place of a
    comparison...)
    
    Fixed a typo where the database name was incorrect.
    (session-remider-app_db.sqlite -> session-reminder-app_db.sqlite)
    
    Removed commented code.
    
    Added date format to /session create slash subcommand.

--- src/classes/Database/index.ts
+++ src/classes/Database/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 export const database: Sequelize = new Sequelize({
 	dialect: "sqlite",
-	storage: "session-remider-app_db.sqlite",
+	storage: "session-reminder-app_db.sqlite",
 	logging: false,
 	define: { underscored: true }	
 });
